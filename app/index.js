e1919847986bf3bded094b89d7efbc6717cf744a
dgriego <eggomygriego@gmail.com>
2019-09-18 18:23:22 -0700

    first commit

---
+++ app/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+// each component uses state to compose itself
+// Component
+// State
+// Lifecycle
+// UI
+
+function App() {
+    return (
+        <div>hello fart</div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
